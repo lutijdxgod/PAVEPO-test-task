47ef04a6b92007bd2643eef85691f897b08269f2
lutijdxgod <den31804@mail.ru>
2025-03-30 00:24:30 +0300

    added config.py file

--- app/config.py
+++ app/config.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Database(BaseModel):
@@ -15,3 +16,21 @@ class Database(BaseModel):
         "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
         "pk": "pk_%(table_name)s",
     }
+
+
+class Yandex(BaseModel):
+    client_id: str
+    client_secret: str
+    redirect_uri: str
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env", env_file_encoding="utf-8", env_nested_delimiter="__"
+    )
+
+    db: Database
+    yandex: Yandex
+
+
+settings = Settings()
