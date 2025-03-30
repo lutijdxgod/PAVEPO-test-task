8695b916ded20e2d92a047a7da395fdc548fa782
lutijdxgod <den31804@mail.ru>
2025-03-30 03:31:03 +0300

    added router to get audiofile info by id

--- app/api/audiofiles/router.py
+++ app/api/audiofiles/router.py
@@ -2,12 +2,14 @@ from pathlib import Path
 import shutil
 from fastapi import APIRouter, Depends, HTTPException, Query, UploadFile
 from app.api.users.utils import validate_audio_file
+from app.functions.crud_helpers import get_single_entity_by_field
 from app.models import models
 from app.models.database import db_helper as db
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.oauth2 import get_current_user
+from app.schemas.audiofiles import FileOut
 from app.schemas.auth import UserOut
 
 router = APIRouter(prefix="/audiofiles", tags=["Audiofiles"])
@@ -48,3 +50,16 @@ async def upload_audiofile(
     await session.commit()
 
     return {"filename": new_filename, "message": "Файл успешно загружен."}
+
+
+@router.get("/{id}", response_model=FileOut)
+async def get_audiofile_by_id(
+    id: int,
+    session: AsyncSession = Depends(db.session_getter),
+    current_user: UserOut = Depends(get_current_user),
+):
+    file = await get_single_entity_by_field(
+        models.AudioFile, models.AudioFile.id, id, session
+    )
+
+    return file
